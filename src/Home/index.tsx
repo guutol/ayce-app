dd5020caf060063428033c081f821bda8cdafc9f
Gustavo Bomfim <n4megustavo@gmail.com>
2026-02-03 21:24:27 -0300

    chore: adição de logo e texto inicial

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,6 +1,13 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import { Button } from "@rneui/themed";
+import { styles } from "@/Home/styles";
 
 export function Home() {
-  return <View></View>;
+  return (
+    <View style={[styles.container]}>
+      <Image source={require("@/assets/logo.png")} style={[styles.logo]} />
+      <Text style={[styles.title]}>Contador de Rodízio</Text>
+      <Text style={[styles.subtitle]}>Escolha o tipo de rodízio</Text>
+    </View>
+  );
 }
---
+++ src/Home/styles.ts
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "white",
+    paddingTop: 62,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 600,
+  },
+  subtitle: {
+    fontSize: 20,
+    padding: 10,
+  },
+  logo: {
+    width: 300,
+    height: 70,
+  },
+});
